d9234ba98635fb99dfb367ead49087d72a47667b
Paula <torreblancapaula@gmail.com>
2025-12-07 16:58:45 -0500

    changes

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -12,9 +12,15 @@ urlpatterns = [
     path(route='login', view=views.login_user, name='login'),
 
     # path for dealer reviews view
+    path(route='get_dealers/', view=views.get_dealerships, name='get_dealers'),
+    path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
 
     # path for add a review view
 
     path(route='get_cars', view=views.get_cars, name ='getcars'),
+    path(route='dealer/<int:ddealer_id>', view=views.get_dealer_details, name='dealer_details'),
+    path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
+    path(route='add_review', view=views.add_review, name='add_review'), 
+    path('get_dealers/', views.get_dealerships, name='get_dealers'),  # <-- note the slash
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
