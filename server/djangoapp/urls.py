186530b7b7fc4ea6b64ec858ffefabedd7da723b
Paula <torreblancapaula@gmail.com>
2025-12-08 02:23:35 -0500

    added user management

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -7,9 +7,11 @@ from . import views
 app_name = 'djangoapp'
 urlpatterns = [
     # # path for registration
-
+    path(route='register', view=views.registration, name='register'),
+    
     # path for login
     path(route='login', view=views.login_user, name='login'),
+    path(route='logout', view=views.logout_request, name='logout'),
 
     # path for dealer reviews view
     path(route='get_dealers/', view=views.get_dealerships, name='get_dealers'),
